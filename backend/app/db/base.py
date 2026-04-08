3c0d6be3b66ad6af5017cc828d16fb1c00caf301
Zafer Akkan <47734158+Zaferius@users.noreply.github.com>
2026-04-08 13:58:16 +0300

    upated base

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import declarative_base
-from app.models.job import Job  # noqa: E402
-from app.models.user import User  # noqa: E402
 
 Base = declarative_base()
+
+from app.models.job import Job  # noqa: E402
+from app.models.user import User  # noqa: E402

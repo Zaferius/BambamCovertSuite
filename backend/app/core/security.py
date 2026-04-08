66bfc0615e3c06a099fd26166240036ec27cd3b8
Zafer Akkan <47734158+Zaferius@users.noreply.github.com>
2026-04-08 14:03:32 +0300

    updated sec

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta, timezone
+from typing import Any
 
 from jose import jwt
 from passlib.context import CryptContext
@@ -18,7 +19,7 @@ def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 
-def create_access_token(subject: str | any, expires_delta: timedelta | None = None) -> str:
+def create_access_token(subject: Any, expires_delta: timedelta | None = None) -> str:
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
